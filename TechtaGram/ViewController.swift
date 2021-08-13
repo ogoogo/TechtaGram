fd4f285eefde87c1bd1e08a947aed0671417b457
ogoogo <ogosae.1018173@icloud.com>
2021-08-13 11:37:20 +0900

    Initial Commit

---
+++ TechtaGram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechtaGram
+//
+//  Created by 生越冴恵 on 2021/08/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
